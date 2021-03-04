881614e8013432ee816f6fd6f7d6b28b384650e2
PotatoAim101 <juliette.rossie@gmail.com>
2021-03-04 08:27:47 +0100

    seperated play for main

--- main_prog.py
+++ main_prog.py
@@ -1,42 +1,34 @@
 from car_racing import *
+from pyglet.window import key
 
-if __name__ == "__main__":
-    from pyglet.window import key
 
-    a = np.array([0.0, 0.0, 0.0])
+def key_press(k, mod):
+    if k == 0xFF0D:
+        restart = True
+    if k == key.LEFT:
+        a[0] = -1.0
+    if k == key.RIGHT:
+        a[0] = +1.0
+    if k == key.UP:
+        a[1] = +1.0
+    if k == key.DOWN:
+        a[2] = +0.8  # set 1.0 for wheels to block to zero rotation
 
-    def key_press(k, mod):
-        global restart
-        if k == 0xFF0D:
-            restart = True
-        if k == key.LEFT:
-            a[0] = -1.0
-        if k == key.RIGHT:
-            a[0] = +1.0
-        if k == key.UP:
-            a[1] = +1.0
-        if k == key.DOWN:
-            a[2] = +0.8  # set 1.0 for wheels to block to zero rotation
 
-    def key_release(k, mod):
-        if k == key.LEFT and a[0] == -1.0:
-            a[0] = 0
-        if k == key.RIGHT and a[0] == +1.0:
-            a[0] = 0
-        if k == key.UP:
-            a[1] = 0
-        if k == key.DOWN:
-            a[2] = 0
+def key_release(k, mod):
+    if k == key.LEFT and a[0] == -1.0:
+        a[0] = 0
+    if k == key.RIGHT and a[0] == +1.0:
+        a[0] = 0
+    if k == key.UP:
+        a[1] = 0
+    if k == key.DOWN:
+        a[2] = 0
 
-    env = CarRacing()
-    env.render()
+
+def play(env):
     env.viewer.window.on_key_press = key_press
     env.viewer.window.on_key_release = key_release
-    record_video = False
-    if record_video:
-        from gym.wrappers.monitor import Monitor
-
-        env = Monitor(env, "/tmp/video-test", force=True)
     isopen = True
     while isopen:
         env.reset()
@@ -53,4 +45,21 @@ if __name__ == "__main__":
             isopen = env.render()
             if done or restart or isopen == False:
                 break
+
+
+if __name__ == "__main__":
+    global restart
+
+    a = np.array([0.0, 0.0, 0.0])
+
+    env = CarRacing()
+    env.render()
+    record_video = False
+    if record_video:
+        from gym.wrappers.monitor import Monitor
+
+        env = Monitor(env, "/tmp/video-test", force=True)
+
+    play(env)
+
     env.close()
