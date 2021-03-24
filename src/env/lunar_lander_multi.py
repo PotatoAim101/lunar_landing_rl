a328956076a8f8d8a8e8c730cfdbd8d6e9e7d56d
guillaumeHoury <guillaume.houry@live.fr>
2021-03-24 21:09:03 +0100

    Added multi lander and trained models

--- src/env/lunar_lander_multi.py
+++ src/env/lunar_lander_multi.py
@@ -71,7 +71,7 @@ class ContactDetector(contactListener):
                 self.env.all_legs[self.n][i].ground_contact = False
 
 
-class LunarLander(gym.Env, EzPickle):
+class LunarLanderMulti(gym.Env, EzPickle):
     metadata = {
         'render.modes': ['human', 'rgb_array'],
         'video.frames_per_second' : FPS
@@ -418,72 +418,5 @@ class LunarLander(gym.Env, EzPickle):
             self.viewer = None
 
 
-class LunarLanderContinuous(LunarLander):
+class LunarLanderMultiContinuous(LunarLanderMulti):
     continuous = True
-
-def heuristic(env, s):
-    """
-    The heuristic for
-    1. Testing
-    2. Demonstration rollout.
-    Args:
-        env: The environment
-        s (list): The state. Attributes:
-                  s[0] is the horizontal coordinate
-                  s[1] is the vertical coordinate
-                  s[2] is the horizontal speed
-                  s[3] is the vertical speed
-                  s[4] is the angle
-                  s[5] is the angular speed
-                  s[6] 1 if first leg has contact, else 0
-                  s[7] 1 if second leg has contact, else 0
-    returns:
-         a: The heuristic to be fed into the step function defined above to determine the next step and reward.
-    """
-
-    angle_targ = s[0]*0.5 + s[2]*1.0         # angle should point towards center
-    if angle_targ > 0.4: angle_targ = 0.4    # more than 0.4 radians (22 degrees) is bad
-    if angle_targ < -0.4: angle_targ = -0.4
-    hover_targ = 0.55*np.abs(s[0])           # target y should be proportional to horizontal offset
-
-    angle_todo = (angle_targ - s[4]) * 0.5 - (s[5])*1.0
-    hover_todo = (hover_targ - s[1])*0.5 - (s[3])*0.5
-
-    if s[6] or s[7]:  # legs have contact
-        angle_todo = 0
-        hover_todo = -(s[3])*0.5  # override to reduce fall speed, that's all we need after contact
-
-    if env.continuous:
-        a = np.array([hover_todo*20 - 1, -angle_todo*20])
-        a = np.clip(a, -1, +1)
-    else:
-        a = 0
-        if hover_todo > np.abs(angle_todo) and hover_todo > 0.05: a = 2
-        elif angle_todo < -0.05: a = 3
-        elif angle_todo > +0.05: a = 1
-    return a
-
-def demo_heuristic_lander(env, seed=None, render=False):
-    env.seed(seed)
-    total_reward = 0
-    steps = 0
-    s = env.reset()
-    while True:
-        a = heuristic(env, s)
-        s, r, done, info = env.step(a)
-        total_reward += r
-
-        if render:
-            still_open = env.render()
-            if still_open == False: break
-
-        if steps % 20 == 0 or done:
-            print("observations:", " ".join(["{:+0.2f}".format(x) for x in s]))
-            print("step {} total_reward {:+0.2f}".format(steps, total_reward))
-        steps += 1
-        if done: break
-    return total_reward
-
-
-if __name__ == '__main__':
-    demo_heuristic_lander(LunarLander(), render=True)
